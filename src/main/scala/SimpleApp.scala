c9be79ac0e68f88021826336b5c326eeb790dc00
Patrick Madden <maddenpj@gmail.com>
2020-06-10 10:17:21 -0700

    Fixed spark scala versions

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -77,7 +77,7 @@ object SimpleApp {
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
    spark.sparkContext.setLogLevel("OFF")
     //val inputfile = sc.textFile("/home/faiz/Projects/Kaggle/Titanic/titanic/test.csv")
-    val inputfile = spark.read.format("csv").option("sep",",").option("inferSchema","true").option("header","true").load("/home/faiz/Projects/Kaggle/Titanic/train.csv")
+    val inputfile = spark.read.format("csv").option("sep",",").option("inferSchema","true").option("header","true").load("/Users/patrickmadden/src/patrick/titanic/train.csv")
     inputfile.printSchema()
 
     inputfile.filter( inputfile("Survived") === "1").show()
