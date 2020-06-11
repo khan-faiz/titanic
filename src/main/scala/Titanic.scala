ee4a2e2e5236cdb9415e33a38f0ed3778b1fa633
khan-faiz <faizkhan00@gmail.com>
2020-06-11 02:37:42 +0000

    added some null analysis

--- src/main/scala/Titanic.scala
+++ src/main/scala/Titanic.scala
@@ -17,37 +17,52 @@ object Titanic {
       .load("src/main/resources/train.csv")
     inputfile.printSchema()
 
+    println(inputfile.getClass)
+    basicAnalysis(inputfile)
+
+  }
+  def basicAnalysis(inputfile: org.apache.spark.sql.DataFrame) {
     inputfile.filter( inputfile("Survived") === "1").show()
 
-   // inputfile.filter( x => x( x.fieldIndex("Survived") ) == 1 ).show() // Alt way
-
     val totalrecords = inputfile.count()
-    println(totalrecords)
 
     var row = inputfile.take(5)(3)
-    println(row)
-    println(row.fieldIndex("Age"))
-    println(row( row.fieldIndex("Age") ) )
-    println(row( row.fieldIndex("Age") )  == null )
-    println( inputfile.filter( x => x( x.fieldIndex("Age") ) == null ).getClass)
 
+    val nPID = nullChecks( inputfile, row.fieldIndex("PassengerId") )
+    println(s"PID: Found $nPID null entries, $totalrecords total entries, ${ 100*nPID.toFloat/totalrecords }% null")
+
+    val nPclass = nullChecks( inputfile, row.fieldIndex("Pclass") )
+    println(s"Pclass: Found $nPclass null entries, $totalrecords total entries, ${ 100*nPclass.toFloat/totalrecords }% null")
   
-    val noage = inputfile.filter( x => x( x.fieldIndex("Age") ) == null )
+    val nName = nullChecks( inputfile, row.fieldIndex("Name") )
+    println(s"Name: Found $nName null entries, $totalrecords total entries, ${ 100*nName.toFloat/totalrecords }% null")
 
-    println(noage)
-    println("test")
-    println(noage.take(5))
-    noage.show() 
-    //println(noage)
-    val nocabin = inputfile.filter( x=> x( x.fieldIndex("Cabin") ) == null )
-    nocabin.show()
-    println(nocabin)
+    val nSex = nullChecks( inputfile, row.fieldIndex("Sex") ) //ಠ_ಠ
+    println(s"Sex: Found $nSex null entries, $totalrecords total entries, ${ 100*nSex.toFloat/totalrecords }% null")
 
-    println(totalrecords)
-/*    println(noage/totalrecords)
-    println(nocabin/totalrecords)
+    val nAges = nullChecks( inputfile, row.fieldIndex("Age") )
+    println(s"Ages: Found $nAges null entries, $totalrecords total entries, ${ 100*nAges.toFloat/totalrecords }% null")
 
-    inputfile.filter( x=> x( x.fieldIndex("Cabin") ) == null ).count()*/
+    val nSib = nullChecks( inputfile, row.fieldIndex("SibSp") )
+    println(s"Siblings: Found $nSib null entries, $totalrecords total entries, ${ 100*nSib.toFloat/totalrecords }% null")
 
+    val nParch = nullChecks( inputfile, row.fieldIndex("Parch") )
+    println(s"Parchment: Found $nParch null entries, $totalrecords total entries, ${ 100*nParch.toFloat/totalrecords }% null")
+
+    val nTick = nullChecks( inputfile, row.fieldIndex("Ticket") )
+    println(s"Ticket: Found $nTick null entries, $totalrecords total entries, ${ 100*nTick.toFloat/totalrecords }% null")
+
+    val nFare = nullChecks( inputfile, row.fieldIndex("Fare") )
+    println(s"Fare: Found $nFare null entries, $totalrecords total entries, ${ 100*nFare.toFloat/totalrecords }% null")
+
+    val nCabin = nullChecks( inputfile, row.fieldIndex("Cabin") )
+    println(s"Cabin: Found $nCabin null entries, $totalrecords total entries, ${ 100*nCabin.toFloat/totalrecords }% null")
+
+    val nEmb = nullChecks( inputfile, row.fieldIndex("Embarked") )
+    println(s"Embarked: Found $nEmb null entries, $totalrecords total entries, ${ 100*nEmb.toFloat/totalrecords }% null")
+
+  }
+  def nullChecks(df: org.apache.spark.sql.DataFrame, index: Int) : Long = {
+    df.filter( x => x( index ) == null ).count()
   }
 }
