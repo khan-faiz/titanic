cfdc2803766a8a2243e0e76031db9e2cfe07c5af
khan-faiz <faizkhan00@gmail.com>
2020-06-12 04:09:47 +0000

    Doesn't compile but added initial transformation code

--- src/main/scala/Titanic.scala
+++ src/main/scala/Titanic.scala
@@ -2,8 +2,13 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.mllib.classification.{SVMModel,SVMWithSGD}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.linalg.{Vector,Vectors}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+
+
+case class VectorRecord( label: Int, features: org.apache.spark.ml.linalg.Vector)
 
 object Titanic {
 
@@ -13,6 +18,7 @@ object Titanic {
     Logger.getLogger("akka").setLevel(Level.OFF)
 
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    import spark.implicits._
     spark.sparkContext.setLogLevel("OFF")
     val input = spark
       .read
@@ -24,7 +30,21 @@ object Titanic {
     input.printSchema()
 
     println(input.getClass)
-    basicAnalysis(input)
+    //basicAnalysis(input)
+    val transformed = inputMassage(input)
+    //val transformed = input.map( row => ( row( row.fieldIndex("Survived") ), 1 ) )
+    //val transformed = input.map( row => ( 1 , 1 ) )
+    println(transformed)
+
+
+  }
+  def inputMassage(input: org.apache.spark.sql.DataFrame) = {
+     //println(input.take(4)(2)( input.take(4)(2).fieldIndex("Survived")))
+     //println(input.take(4)(2)( input.take(4)(2).fieldIndex("Survived")).getClass)
+     //input.map( row => ( row( row.fieldIndex("Survived") ), row( row.fieldIndex("PassengerID") ) ) )
+     //input.map( row => ( row( row.fieldIndex("Survived") ), Vectors.dense(1.0) ):(Int, Vector) )
+     input.map( row => ( 4, Vectors.dense(1.0) ):(Int, Vector) )
+
   }
 
   def basicAnalysis(input: org.apache.spark.sql.DataFrame) {
