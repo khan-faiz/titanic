efebc6e64f5a27de60b7f1d143d9bf101df20320
khan-faiz <faizkhan00@gmail.com>
2020-06-19 01:19:21 +0000

    Uhhh 99% survival rate, somethings not right...

--- src/main/scala/Titanic.scala
+++ src/main/scala/Titanic.scala
@@ -34,11 +34,27 @@ object Titanic {
     println(input.getClass)
     basicAnalysis(input, 0)
 
-    val inputlist = input.takeAsList( input.count().toInt ).asScala.toList
+    //Could factor this, basically some meta shit for later
+    val metadata = input.groupBy().agg( "Age" -> "avg", "Fare" -> "max") 
+    val avgage = metadata.first().getDouble(0)
+    val faremax = metadata.first().getDouble(1) 
+    val newinput = input.na.fill(avgage, Seq("Age") ).withColumn("Fare", input("Fare") / faremax )
+
+    // check for no age nulls
+    basicAnalysis(newinput,0)
+
+    //retard way to average - DONT DO THIS, use describe()
+    //val agesavg = ages.reduce( (row1, row2) => Row( row1.getDouble(0) + row2.getDouble(0)) ).getDouble(0) / ages.count()
+    //retard way to average #2 - DONT DO THIS, use agg()
+    //val agesdata = input.select("Age").filter( row => row( row.fieldIndex("Age") ) != null ).describe()
+    //val ageavg = agesdata.collect()(1).getDouble(1) //1st row is mean
+
+    val inputlist = newinput.takeAsList( newinput.count().toInt ).asScala.toList
     val transformed = inputMassage(inputlist, 0)
 
+
     //NOTE: If we wanted to convert to RDD from List, this is how
-    val inputrdd = spark.sparkContext.parallelize(transformed,1)
+    //val inputrdd = spark.sparkContext.parallelize(transformed,1)
     //inputrdd.take(4).foreach(println)
 
     //create dataframe from massaged data
@@ -59,10 +75,17 @@ object Titanic {
       .option("inferSchema","true")
       .option("header","true")
       .load("src/main/resources/test.csv")
-    testinput.printSchema()
 
     //testinput has no survived column, so pass '1' as param to indicate not to mess with this
     basicAnalysis(testinput, 1)
+
+    /*
+    val testmetadata = testinput.groupBy().agg( "Age" -> "avg", "Fare" -> "max", "Fare" -> "avg") 
+    val testavgage = testmetadata.first().getDouble(0)
+    val testfaremax = testmetadata.first().getDouble(1) 
+    val testfareavg = testmetadata.first().getDouble(2) 
+    val testnewinput = testinput.na.fill(testavgage, Seq("Age") ).withColumn("Fare", input("Fare") / testfaremax ).na.fill(testfareavg, Seq("Fare") )
+    */
 
     //massage test data like train
     val testinputlist = testinput.takeAsList( testinput.count().toInt ).asScala.toList
@@ -73,6 +96,10 @@ object Titanic {
 
     //actual fitting to earlier model
     val results = model1.transform(testinputdf)
+
+    //print params
+    //println(model1.explainParams())
+    //println(model1.params.foreach(x=> println(x.toString)))
 
     //There is a 'probability' column that exists but we don't care for this attempt
     /*results.select("features","probability","prediction").collect()
@@ -94,16 +121,18 @@ object Titanic {
 
   }
   def inputMassage(input: List[org.apache.spark.sql.Row], testdata: Int) = {
+
       //if testdata == 1, then we ignore survived column
-      input.map { row =>
+      val formatData = input.map { row =>
         val label = if( testdata == 0) row.getInt(row.fieldIndex("Survived")) else 0
-        val featureCols = row.schema.fieldNames.filter( x => ( (x != "Survived") && (x != "Name") && (x != "Age") && (x != "Cabin") && (x != "Ticket") && (x != "Embarked") ) ) 
+        val featureCols = row.schema.fieldNames.filter( x => ( (x != "Survived") && (x != "Name")  && (x != "Cabin") && (x != "Ticket") && (x != "Embarked") ) ) 
         // need to properly handle embarked, age, and ticket at some point instead of throwing it away, TODO
         (label, row.getValuesMap(featureCols) )
-      }.map{ row => 
-        
+      }
+
+      formatData.map{ row => 
           val sex = if (row._2("Sex").toString == "male") 1 else 2
-          (row._1, Vectors.dense( row._2("PassengerId"):Int, row._2("Pclass"):Int, row._2("SibSp"):Int, row._2("Parch"):Int,row._2("Fare") , sex ) ) 
+          (row._1, Vectors.dense( row._2("PassengerId"):Int, row._2("Pclass"):Int, row._2("SibSp"):Int, row._2("Age"), row._2("Parch"):Int,row._2("Fare") , sex ) ) 
       }
 
   }
